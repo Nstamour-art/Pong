836886887d776a3850610cee62098af9022f8558
Nicolas St-Amour <nstamour.dev@outlook.com>
2026-01-08 08:53:12 -0500

    Complete Ball script and add marker 2d as starting point for ball
    Fixed paddle rebound functionality and updated where the ball starts.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,16 +2,37 @@ extends CharacterBody2D
 
 
 @export var SPEED = 300.0
+@export var marker: Marker2D
+
+func _ready() -> void:
+    reset_ball()
 
 
 func reset_ball() -> void:
-    position = get_viewport_rect().size / 2
+    position = marker.global_position
     
-    var x_dir = [-1, 1].pick_random()
-    var y_dir = randf_range(-0.5, 0.5)
+    var direction: Vector2
     
-    velocity = Vector2(x_dir, y_dir).normalized() * SPEED
+    direction.x = [-1, 1].pick_random()
+    direction.y = randf_range(-0.5, 0.5)
+    
+    velocity = direction.normalized() * SPEED
 
 
 func _physics_process(delta: float) -> void:
-    pass
+    var collision = move_and_collide(velocity * delta)
+    
+    if collision:
+        var collider = collision.get_collider()
+        print(collider.name)
+        
+        if collider.is_in_group("paddles"):
+            var paddle_shape = collision.get_collider_shape()
+            var paddle_height = paddle_shape.shape.height
+            var relative_y = (global_position.y - collider.global_position.y) / (paddle_height / 2)
+            
+            var new_direction = Vector2(-sign(velocity.x), relative_y)
+            velocity = new_direction.normalized() * SPEED
+            
+        else:
+            velocity = velocity.bounce(collision.get_normal()).normalized() * SPEED
